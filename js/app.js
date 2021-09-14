bc6bc13c3f3e35190b1aed2d19568724092f3330
showkhin1997 <showkhin15@gmail.com>
2021-09-14 11:35:22 +0600

    image slider

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const images = [
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg",
+    "images/7.jpg",
+    "images/8.jpg",
+    "images/9.jpg",
+];
+
+let imageIndex = 0;
+const imgElement = document.getElementById('slider-img');
+setInterval(() => {
+    if (imageIndex >= images.length) {
+        imageIndex = 0;
+    }
+    const imgUrl = images[imageIndex];
+    imgElement.setAttribute('src', imgUrl);
+    imageIndex++;
+}, 2000)
